ca77102708a13b69794b29c80858c68d06ce05c7
Ralph Simon <ralph.simon2012@icloud.com>
2019-05-12 13:00:17 +0200

    Add ref forwarding for buttons

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,4 +1,7 @@
+import { forwardRef } from 'react'
+
 interface ButtonProps {
+  ariaLabel: string;
   color?: string;
   disabled: boolean;
   iconBefore: JSX.Element | HTMLElement | SVGElement;
@@ -9,135 +12,142 @@ interface ButtonProps {
   type?: string;
 }
 
-export const Button = ({
-  color,
-  disabled,
-  iconBefore,
-  iconAfter,
-  label,
-  onClick,
-  outline,
-  type
-}: ButtonProps) => {
-  const btnClass: string = `btn btn__base ${
-    outline ? 'btn--outline' : 'btn--filled'
-  }`
+export const Button = forwardRef(
+  (
+    {
+      ariaLabel,
+      color,
+      disabled,
+      iconBefore,
+      iconAfter,
+      label,
+      onClick,
+      outline,
+      type
+    }: ButtonProps,
+    ref
+  ) => {
+    const btnClass: string = `btn btn__base ${
+      outline ? 'btn--outline' : 'btn--filled'
+    }`
 
-  return (
-    <button
-      className={btnClass}
-      onClick={onClick}
-      disabled={disabled}
-      type={type}>
-      {iconBefore ? iconBefore : null}
-      <span className="label">{label}</span>
-      {iconAfter ? iconAfter : null}
+    return (
+      <button
+        aria-label={ariaLabel}
+        className={btnClass}
+        onClick={onClick}
+        disabled={disabled}
+        ref={ref}
+        type={type}>
+        {iconBefore ? iconBefore : null}
+        <span className="label">{label}</span>
+        {iconAfter ? iconAfter : null}
 
-      <style jsx>{`
-				.btn {
-					--bg-color: ${`var(--color-${color}-base)`};
-				}
-			`}</style>
+        <style jsx>{`
+					.btn {
+						--bg-color: ${`var(--color-${color}-base)`};
+					}
+				`}</style>
 
-      <style jsx>{`
-				.btn {
-					display: inline-flex;
-					align-items: center;
-					justify-content: center;
-					border: none;
-					font-family: inherit;
-					outline: none;
-					vertical-align: middle;
-					user-select: none;
-					-webkit-appearance: none;
-					cursor: pointer;
-					opacity: 0.87;
-				}
+        <style jsx>{`
+					.btn {
+						display: inline-flex;
+						align-items: center;
+						justify-content: center;
+						border: none;
+						font-family: inherit;
+						outline: none;
+						vertical-align: middle;
+						user-select: none;
+						-webkit-appearance: none;
+						cursor: pointer;
+						opacity: 0.87;
+					}
 
-				.label {
-					margin: 0 8px;
-				}
+					.label {
+						margin: 0 8px;
+					}
 
-				.btn__base {
-					height: 42px;
-					min-width: 64px;
-					padding: 0 24px;
-					border-radius: 4px;
-					font-size: 0.875rem;
-					font-weight: 800;
-					word-spacing: 0.275em;
-					letter-spacing: 0.15em;
-					text-transform: uppercase;
-				}
+					.btn__base {
+						height: 42px;
+						min-width: 64px;
+						padding: 0 24px;
+						border-radius: 4px;
+						font-size: 0.875rem;
+						font-weight: 800;
+						word-spacing: 0.275em;
+						letter-spacing: 0.15em;
+						text-transform: uppercase;
+					}
 
-				.btn--filled {
-					color: var(--color-text-white);
-					background-color: var(--bg-color);
-				}
+					.btn--filled {
+						color: var(--color-text-white);
+						background-color: var(--bg-color);
+					}
 
-				.btn--filled:focus,
-				.btn--filled:hover {
-					box-shadow: 0 2px 4px rgba(0, 0, 0, 0.24);
-					opacity: 1;
-				}
+					.btn--filled:focus,
+					.btn--filled:hover {
+						box-shadow: 0 2px 4px rgba(0, 0, 0, 0.24);
+						opacity: 1;
+					}
 
-				.btn--filled:disabled {
-					--bg-color: var(--color-gray-base);
-					opacity: 0.54;
-					cursor: not-allowed;
-					box-shadow: none;
-				}
+					.btn--filled:disabled {
+						--bg-color: var(--color-gray-base);
+						opacity: 0.54;
+						cursor: not-allowed;
+						box-shadow: none;
+					}
 
-				.btn--outline {
-					position: relative;
-					color: var(--bg-color);
-					background-color: transparent;
-					border: 2px solid var(--bg-color);
-					overflow: hidden;
-				}
+					.btn--outline {
+						position: relative;
+						color: var(--bg-color);
+						background-color: transparent;
+						border: 2px solid var(--bg-color);
+						overflow: hidden;
+					}
 
-				.btn--outline::before {
-					z-index: -1;
-					content: '';
-					position: absolute;
-					top: 0;
-					left: 0;
-					width: 100%;
-					height: 100%;
-					background-color: var(--bg-color);
-					transform: translate(-100%, 0);
-					transition: transform 0.25s var(--easing-default);
-				}
+					.btn--outline::before {
+						z-index: -1;
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+						background-color: var(--bg-color);
+						transform: translate(-100%, 0);
+						transition: transform 0.25s var(--easing-default);
+					}
 
-				.btn--outline:hover,
-				.btn--outline:focus {
-					color: var(--color-text-white);
-				}
-				.btn--outline:hover::before,
-				.btn--outline:focus::before {
-					transform: translate(0, 0);
-				}
+					.btn--outline:hover,
+					.btn--outline:focus {
+						color: var(--color-text-white);
+					}
+					.btn--outline:hover::before,
+					.btn--outline:focus::before {
+						transform: translate(0, 0);
+					}
 
-				.btn--outline:disabled,
-				.btn--outline:disabled:hover {
-					color: var(--color-gray-base);
-					border-color: var(--color-gray-base);
-					cursor: not-allowed;
-					opacity: 0.54;
-				}
+					.btn--outline:disabled,
+					.btn--outline:disabled:hover {
+						color: var(--color-gray-base);
+						border-color: var(--color-gray-base);
+						cursor: not-allowed;
+						opacity: 0.54;
+					}
 
-				.btn--outline:disabled::before,
-				.btn--outline:disabled:hover::before {
-					opacity: 0;
-					transform: none;
-				}
-			`}</style>
-    </button>
-  )
-}
+					.btn--outline:disabled::before,
+					.btn--outline:disabled:hover::before {
+						opacity: 0;
+						transform: none;
+					}
+				`}</style>
+      </button>
+    )
+  }
+)
 
 Button.defaultProps = {
   color: 'indigo',
-  type: 'button',
-  outline: true
+  type: 'button'
 }
--- components/Buttons/IconButton.tsx
+++ components/Buttons/IconButton.tsx
@@ -1,4 +1,7 @@
+import { forwardRef } from 'react'
+
 interface IconButtonProps {
+  ariaLabel: string;
   children: JSX.Element[] | JSX.Element;
   color?: string;
   filled: boolean;
@@ -7,107 +10,118 @@ interface IconButtonProps {
   title: string;
 }
 
-export const IconButton = ({
-  children,
-  color,
-  filled,
-  onClick,
-  size,
-  title
-}: IconButtonProps) => {
-  const btnClass = `btn ${filled ? 'btn--filled' : 'btn--base'}`
+export const IconButton = forwardRef(
+  (
+    {
+      ariaLabel,
+      children,
+      color,
+      filled,
+      onClick,
+      size,
+      title
+    }: IconButtonProps,
+    ref
+  ) => {
+    const btnClass = `btn ${filled ? 'btn--filled' : 'btn--base'}`
 
-  return (
-    <button className={btnClass} title={title} onClick={onClick}>
-      <span className="icon">{children}</span>
-      <style jsx>{`
-				.btn {
-					--size: ${size}px;
-					--color-primary: ${`var(--color-${color}-dark)`};
-					--color-secondary: ${`var(--color-${color}-light)`};
-					--color-focus: ${`var(--color-${color}-base)`};
-				}
-			`}</style>
-      <style jsx>{`
-				.btn {
-					z-index: 1;
-					position: relative;
-					display: inline-flex;
-					align-items: center;
-					justify-content: center;
-					width: var(--size);
-					height: var(--size);
-					padding: 0;
-					border: none;
-					border-radius: 100%;
-					font-family: inherit;
-					outline: none;
-					vertical-align: middle;
-					user-select: none;
-					-webkit-appearance: none;
-					cursor: pointer;
-				}
+    return (
+      <button
+        aria-label={ariaLabel}
+        className={btnClass}
+        title={title}
+        onClick={onClick}
+        ref={ref}>
+        <span className="icon">{children}</span>
+        <style jsx>{`
+					.btn {
+						--size: ${size}px;
+						--color-primary: ${`var(--color-${color}-dark)`};
+						--color-secondary: ${`var(--color-${color}-light)`};
+						--color-focus: ${`var(--color-${color}-base)`};
+					}
+				`}</style>
+        <style jsx>{`
+					.btn {
+						z-index: 1;
+						position: relative;
+						display: inline-flex;
+						align-items: center;
+						justify-content: center;
+						width: var(--size);
+						height: var(--size);
+						padding: 0;
+						border: none;
+						border-radius: 100%;
+						font-family: inherit;
+						outline: none;
+						vertical-align: middle;
+						user-select: none;
+						-webkit-appearance: none;
+						cursor: pointer;
+					}
 
-				.icon {
-					z-index: 1;
-				}
+					.icon {
+						z-index: 1;
+					}
 
-				.btn--base {
-					color: var(--color-text-gray);
-					background-color: transparent;
-				}
+					.btn--base {
+						color: var(--color-text-gray);
+						background-color: transparent;
+					}
 
-				.btn--base::before,
-				.btn--filled::before {
-					z-index: 0;
-					content: '';
-					position: absolute;
-					top: 0;
-					left: 0;
-					width: 100%;
-					height: 100%;
-					border-radius: 100%;
-					transform-origin: 50% 50%;
-					transform: scale(0);
-					transition: transform 0.25s var(--easing-default),
-						opacity 0.15s linear;
-				}
+					.btn--base::before,
+					.btn--filled::before {
+						z-index: 0;
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+						border-radius: 100%;
+						transform-origin: 50% 50%;
+						transform: scale(0);
+						transition: transform 0.25s var(--easing-default),
+							opacity 0.15s linear;
+					}
 
-				.btn--base::before {
-					background-color: #ecedf0;
-				}
+					.btn--base::before {
+						background-color: #ecedf0;
+					}
 
-				.btn--base:focus,
-				.btn--base:hover {
-					color: var(--color-focus);
-				}
+					.btn--base:focus,
+					.btn--base:hover {
+						color: var(--color-focus);
+					}
 
-				.btn--filled {
-					color: var(--color-primary);
-					background-color: var(--color-secondary);
-				}
+					.btn--filled {
+						color: var(--color-primary);
+						background-color: var(--color-secondary);
+					}
 
-				.btn--filled:focus,
-				.btn--filled:hover {
-					color: var(--color-text-white);
-				}
+					.btn--filled:focus,
+					.btn--filled:hover {
+						color: var(--color-text-white);
+					}
 
-				.btn--filled::before {
-					opacity: 0;
-					background-color: var(--color-focus);
-				}
+					.btn--filled::before {
+						opacity: 0;
+						background-color: var(--color-focus);
+					}
 
-				.btn--base:focus::before,
-				.btn--base:hover::before,
-				.btn--filled:focus::before,
-				.btn--filled:hover::before {
-					opacity: 1;
-					transform: scale(1);
-				}
-			`}</style>
-    </button>
-  )
-}
+					.btn--base:focus::before,
+					.btn--base:hover::before,
+					.btn--filled:focus::before,
+					.btn--filled:hover::before {
+						opacity: 1;
+						transform: scale(1);
+					}
+				`}</style>
+      </button>
+    )
+  }
+)
 
 IconButton.defaultProps = {
   size: 48,
