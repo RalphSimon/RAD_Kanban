0e01d9ed038d0a6bb7ba9f0416d70f3c4ecc5845
Ralph Simon <ralph.simon2012@icloud.com>
2019-05-29 20:37:28 +0200

    Success! relocator-loader enables deployment with firebase deps

--- firebase/_init/config.ts
+++ firebase/_init/config.ts
@@ -1,19 +1,19 @@
-export const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-}
-
 // export const config = {
-//   apiKey: 'AIzaSyBObEFDb0Vnvb4Cj3C9b7SbcFmzzAVAIrg',
-//   authDomain: 'kanban-b819c.firebaseapp.com',
-//   databaseURL: 'https://kanban-b819c.firebaseio.com',
-//   projectId: 'kanban-b819c',
-//   storageBucket: 'kanban-b819c.appspot.com',
-//   messagingSenderId: '469814446112',
-//   appId: '1:469814446112:web:a63771304e30327b'
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID
 // }
+
+export const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+}
--- next.config.js
+++ next.config.js
@@ -5,20 +5,63 @@ const withTypescript = require('@zeit/next-typescript')
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 
-const nextConfig = {
-  target: 'serverless',
-  webpack: config => {
-    config.plugins = config.plugins || []
-    config.plugins = [
-      ...config.plugins,
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true
-      })
-    ]
-    return config
-  }
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false
+        })
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true
+            }
+          }
+        })
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    }
+  })
 }
 
-module.exports = withTypescript(nextConfig)
+const nextConfig = {
+  target: 'serverless',
+  env: {
+    apiKey: 'AIzaSyBObEFDb0Vnvb4Cj3C9b7SbcFmzzAVAIrg',
+    authDomain: 'kanban-b819c.firebaseapp.com',
+    databaseURL: 'https://kanban-b819c.firebaseio.com',
+    projectId: 'kanban-b819c',
+    storageBucket: 'kanban-b819c.appspot.com',
+    messagingSenderId: '469814446112',
+    appId: '1:469814446112:web:a63771304e30327b'
+  }
+  // webpack: config => {
+  //   config.plugins = config.plugins || []
+  //   config.plugins = [
+  //     ...config.plugins,
+  //     // Read the .env file
+  //     new Dotenv({
+  //       path: path.join(__dirname, '.env'),
+  //       systemvars: true
+  //     })
+  //   ]
+  //   return config
+  // }
+}
+
+module.exports = withTypescript(withAssetRelocator(nextConfig))
